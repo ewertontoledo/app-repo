72d9c5ea502b1f987fe63cea221826562cb32b10
Ewerton Toledo <ewertontoledo1910@gmail.com>
2025-09-01 09:39:16 -0300

    app teste 4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+// src/index.js
 const express = require("express");
 const app = express();
 
 // Rota principal
 app.get("/", (req, res) => {
-  res.send("🧪 Teste do App-teste-3 rodando com sucesso!");
+  res.send("🧪 Teste do App-teste-4 rodando com sucesso!");
 });
 
-// Porta definida via variável de ambiente ou fallback para 9100
-const port = process.env.PORT || 9100;
+// Porta definida via variável de ambiente ou fallback para 9101
+const port = process.env.PORT || 9101;
 
 app.listen(port, "0.0.0.0", () => {
-  console.log(`✅ App-teste-3 rodando na porta ${port}`);
-  console.log(`➡️ Acesse pelo Nginx reverse proxy: http://stage-smart.surf.com.br/teste-3`);
+  console.log(`✅ App-teste-4 rodando na porta ${port}`);
+  console.log(`➡️ Acesse pelo Nginx reverse proxy: http://stage-smart.surf.com.br/teste-4`);
 });
