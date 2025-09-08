9de32e4a7f99c519a6f7c7ae3a5e81458ca5969b
Ewerton Toledo <ewertontoledo1910@gmail.com>
2025-09-08 14:23:08 -0300

    teste imagem docker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 Teste de imagem Docker");
+  res.send("🚀 imagem docker exemplo");
 });
 
 // Porta definida via variável de ambiente ou fallback para 9000
